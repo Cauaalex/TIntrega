16cee445e80ebb109ade62cef7ae50f292370bf1
Cauã Alexandre <cauaalexandrefb777@gmail.com>
2026-08-12 23:43:43 -0300

    Melhorei o nav

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ window.addEventListener("scroll", () => {
         header.classList.remove("scrolled");
     }
 });
+
+const header = document.querySelector("header");
+const hero = document.querySelector(".hero-conteudo");
+
+window.addEventListener("scroll", () => {
+    const heroBottom = hero.offsetTop + hero.offsetHeight;
+
+    if (window.scrollY > 50 && window.scrollY < heroBottom) {
+        // Está sobre o Hero/imagem azul
+        header.classList.add("scrolled");
+        header.classList.remove("white-section");
+
+    } else if (window.scrollY >= heroBottom) {
+        // Saiu da imagem e entrou no fundo branco
+        header.classList.remove("scrolled");
+        header.classList.add("white-section");
+
+    } else {
+        // Está no topo
+        header.classList.remove("scrolled");
+        header.classList.remove("white-section");
+    }
+});
